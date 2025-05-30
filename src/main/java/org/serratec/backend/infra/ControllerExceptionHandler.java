69d5f9891e170b83fcccd43971de86a1d7f4f0dd
EltonKasesky <eltonkasesky9@gmail.com>
2025-05-30 12:58:24 -0300

    Add perfil exception to exception handler

--- src/main/java/org/serratec/backend/infra/ControllerExceptionHandler.java
+++ src/main/java/org/serratec/backend/infra/ControllerExceptionHandler.java
@@ -4,13 +4,16 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.serratec.backend.exception.PerfilException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -36,5 +39,11 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 		ErroResposta erroResposta = new ErroResposta(status.value(), "Há campos inválidos", LocalDateTime.now(), erros);
 
 		return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
+	}
+
+	@ExceptionHandler(PerfilException.class)
+	public ResponseEntity<ErroResposta> handlePerfilException(PerfilException ex) {
+		ErroResposta erroResposta = new ErroResposta(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Foi inserido um valor invalido na requisição!", LocalDateTime.now(), ex.getMessage().lines().toList());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erroResposta);
 	}
 }
